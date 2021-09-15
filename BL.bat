66c8121c513c9a8e9efc61764532759dd2865ab0
john-laythe <john.laythe@gmail.com>
2021-09-15 10:54:19 -0500

    Update BL.bat

--- BL.bat
+++ BL.bat
@@ -1,4 +1,4 @@
-rem Cscript.exe c:\windows\system32\slmgr.vbs /ipk RKN46-Y7Q9Y-CGYPF-926MT-3V64Q
+rem Cscript.exe c:\windows\system32\slmgr.vbs /ipk 
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
