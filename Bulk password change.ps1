48a8b1191d306bbf2fe35004a9c4a7211090eb0c
John Sousa <john.sousa@stmaurice.local>
2021-09-15 10:49:53 -0500

    First commit Message

---
+++ Bulk password change.ps1
@@ -0,0 +1,21 @@
+﻿# Import users from csv file
+
+Import-Csv D:\students.csv |
+
+ForEach-Object {
+
+$samAccountName = $_."samAccountName"
+
+$newPassword = $_."Password"
+
+# Reset user Password.
+
+Set-ADAccountPassword -Identity $samAccountName -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$newPassword" -Force)
+
+# force user to reset passwod at next logon
+
+# Set-ADUser -Identity $samAccountName -ChangePasswordAtLogon $true
+
+Write-Host " Active Directory account Password has been reset for: " $samAccountName
+
+}
